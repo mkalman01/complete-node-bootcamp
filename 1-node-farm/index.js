e1a1bd8908ca26954e3ccf53b2947c9876b1bcac
mkalman01 <mkalman01@gmail.com>
2023-07-01 13:47:39 +0300

    Buidt Simple API and routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /////FILE
@@ -33,10 +34,30 @@ const http = require('http');
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /////SERVER
 
+
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
-    res.end('hello from the server\n oh no');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+    } else if ( pathName === '/product'){
+        res.end('This is the PRODUCT');
+    } else if (pathName === '/api'){      
+        res.writeHead(200, { 'Content-type': 'application/json'});
+        res.end(data);        
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>This page not found</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to req on port 8000');
-})
+});
