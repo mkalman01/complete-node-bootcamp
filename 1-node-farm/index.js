80f3d5b5476340747aa96b3e043ec230e582f79c
mkalman01 <mkalman01@gmail.com>
2023-07-25 16:27:51 +0300

    Added modules in 1-node-farm

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./starter/modules/replaceTemplate");
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /////FILE
@@ -29,61 +30,64 @@ const url = require('url');
 //         });
 //     });
 // });
-// console.log('Will read file!'); 
+// console.log('Will read file!');
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 /////SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  "utf-8"
+);
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // g: global
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-};
-
-const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8')
-
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  "utf-8"
+);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-    // Overview page
-    if(pathName === '/' || pathName === '/overview'){
-        res.writeHead(200, { 'Content-type': 'text/html'});
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
 
-        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-	    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
 
-        res.end(output);
+    res.end(output);
 
     // Product page
-    } else if ( pathName === '/product'){
-        res.end('This is the PRODUCT');
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
     // API
-    } else if (pathName === '/api'){      
-        res.writeHead(200, { 'Content-type': 'application/json'});
-        res.end(data);
-    // Not found  
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('<h1>This page not found</h1>');
-    }
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    // Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>This page not found</h1>");
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to req on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to req on port 8000");
 });
